933377180f1861de9d40ceefcc0847453f638ff6
lietzy <39197778+lietzy@users.noreply.github.com>
2021-10-27 21:55:26 -0500

    Base Structure

---
+++ progam.py
@@ -0,0 +1,18 @@
+
+#Preload data from Movers Table
+#Preload data from Inventory Table
+
+#Get new movers if any in last 10 min
+
+#Update Movers table
+
+#For Each Mover Get historical data
+#Run Logic on True Price
+#Get current price
+#make determination on whether to buy or not
+
+# Look at current inventory if potential Gain > 5% SELL..  Loss > 10% sell
+
+#Log to DB and file any transactions made
+
+#Calculate Net Worth and Update table
